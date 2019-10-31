8f0f7c881853e447ff17e26b76853e88e6a15217
António Santos <xixinhora@gmail.com>
2019-10-31 16:00:33 +0000

    Add index page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import Head from 'next/head'
+import { Layout, Nav } from 'components/organisms'
+import styled from 'styled-components'
 
 const Home = () => (
-  <div>
+  <HomeStyle>
     <Head>
-      <title>Home</title>
+      <title>Liga Nacional de PUBG</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    <h1 className="title">Welcome to Next.js!</h1>
-  </div>
+    <Nav />
+    <Layout>
+      <h1>Liga</h1>
+    </Layout>
+  </HomeStyle>
 )
+
+const Title = styled.p`
+  font-size: ${props => props.theme.sizes.xl8};
+  padding: ${props => props.theme.spacing.s};
+  text-align: center;
+  font-weight: ${props => props.theme.weight.bold};
+`
+
+const HomeStyle = styled.div`
+  span.title-comment {
+    font-weight: ${props => props.theme.weight.xlight};
+  }
+`
 
 export default Home
