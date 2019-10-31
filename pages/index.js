cdad935ba954b6a428ac2bc517f2ab7fa23203ae
António Santos <xixinhora@gmail.com>
2019-10-31 09:23:46 +0000

    :airplane:

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <h1 className="title">Welcome to Next.js!</h1>
+  </div>
+)
+
+export default Home
