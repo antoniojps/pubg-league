0964daacf4a95091fa671c675e7e0d741f5a34be
António Santos <antoniojps@ua.pt>
2019-11-01 01:42:03 +0000

    Fix next config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-/* eslint-disable no-param-reassign */
+/* eslint-disable dot-notation */
 const path = require('path');
 const withCSS = require('@zeit/next-css');
 
 module.exports = withCSS({
   webpack(config) {
-    config.resolve.alias.components = path.join(__dirname, 'components');
-    config.resolve.alias.services = path.join(__dirname, 'services');
-    config.resolve.alias.data = path.join(__dirname, 'data');
+    config.resolve.alias['components'] = path.join(__dirname, 'components');
+    config.resolve.alias['services'] = path.join(__dirname, 'services');
+    config.resolve.alias['data'] = path.join(__dirname, 'data');
     return config;
   },
 });
