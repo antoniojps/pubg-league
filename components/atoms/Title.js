e0f8af2146a2ad5187f9a27d1dd1bde3e436c6c5
António Santos <antoniojps@ua.pt>
2019-11-02 23:00:59 +0000

    Rework title to h1 instead of p

--- components/atoms/Title.js
+++ components/atoms/Title.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 
-const Title = styled.p`
+const Title = styled.h1`
   font-weight: ${(props) => props.theme.weight.bold};
   font-size: ${(props) => props.theme.sizes.xl6};
+  margin: 0;
 `;
 
 export default Title;
