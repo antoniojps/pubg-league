284bbab0df512b0b76c12bbe24ebd19f76547ca0
António Santos <antoniojps@ua.pt>
2019-11-03 01:47:52 +0000

    Add nprogress page loading feedback

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,14 @@ import App from 'next/app';
 import theme from 'services/theme';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from 'services/GlobalStyle';
+import NProgress from 'nprogress';
+import Router from 'next/router';
+
+Router.events.on('routeChangeStart', () => {
+  NProgress.start();
+});
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 class MyApp extends App {
   render() {
