fd3cfbdfe2222e8197c2dae06b3e83f9afa6d6e4
António Santos <antoniojps@ua.pt>
2019-11-11 20:18:03 +0000

    Add reorder to LeaderboardPlayers

--- components/molecules/LeaderboardPlayers.js
+++ components/molecules/LeaderboardPlayers.js
@@ -2,45 +2,73 @@ import React, { useMemo, useState } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { lighten } from 'polished';
-import { Table, TeamLogo, Spacer } from 'components/atoms';
+import {
+  Table, TeamLogo, Spacer, TableHeader, BadgeStat,
+} from 'components/atoms';
 
 const filters = {
   kills: 'kills',
-  damage: 'damage',
   kd: 'kd',
   adr: 'adr',
+  matchesPlayed: 'matchesPlayed',
+};
+
+const orders = {
+  asc: 'asc',
+  desc: 'desc',
 };
 
 const LeaderboardPlayers = ({ playerSummaries }) => {
   const [filter, setFilter] = useState(filters.kills);
+  const [order, setOrder] = useState(orders.asc);
 
   const topPlayers = useMemo(() => {
     const orderedByFilterTopPlayers = playerSummaries.sort((playerA, playerB) => {
       let statA = playerA[filter];
       let statB = playerB[filter];
 
-      if (filter === filters.kd || filter === filters.adr) {
+      if (filter === filters.kd || filter === filters.adr || filter === filters.matchesPlayed) {
         const { computed: computedA } = playerA;
         const { computed: computedB } = playerB;
         statA = computedA[filter];
         statB = computedB[filter];
       }
 
-      if (statA < statB) return 1;
-      if (statA > statB) return -1;
+      if (order === orders.asc) {
+        if (statA < statB) return 1;
+        if (statA > statB) return -1;
+      }
+
+      if (order === orders.desc) {
+        if (statA < statB) return -1;
+        if (statA > statB) return 1;
+      }
+
 
       // when kills are the same order by damage aswell
       if (filter === filters.kills) {
         const damageA = playerA.damage;
         const damageB = playerB.damage;
-        if (damageA < damageB) return 1;
-        if (damageA > damageB) return -1;
+        if (order === orders.asc) {
+          if (damageA < damageB) return 1;
+          if (damageA > damageB) return -1;
+        }
+        if (order === orders.desc) {
+          if (damageA < damageB) return -1;
+          if (damageA > damageB) return 1;
+        }
       }
 
       return 0;
     });
     return orderedByFilterTopPlayers;
-  }, [filter, playerSummaries]);
+  }, [filter, playerSummaries, order]);
+
+  const handleHeaderClick = (nextFilter) => {
+    const nextOrder = order === orders.asc ? orders.desc : orders.asc;
+    setFilter(nextFilter);
+    setOrder(nextOrder);
+  };
 
   return (
     <TablePlayers>
@@ -49,11 +77,37 @@ const LeaderboardPlayers = ({ playerSummaries }) => {
           <tr>
             <th>Lugar</th>
             <th className="team">Jogador</th>
-            <th>Equipa</th>
-            <th>Kills</th>
-            <th>Damage</th>
-            <th>Knocks</th>
-            <th>HS Kills</th>
+            <TableHeader
+              onClick={() => handleHeaderClick(filters.kills)}
+              active={filter === filters.kills}
+              order={order}
+            >
+Kills
+
+            </TableHeader>
+            <TableHeader
+              onClick={() => handleHeaderClick(filters.kd)}
+              active={filter === filters.kd}
+              order={order}
+            >
+K/D
+
+            </TableHeader>
+            <TableHeader
+              onClick={() => handleHeaderClick(filters.adr)}
+              active={filter === filters.adr}
+              order={order}
+            >
+ADR
+
+            </TableHeader>
+            <TableHeader
+              onClick={() => handleHeaderClick(filters.matchesPlayed)}
+              active={filter === filters.matchesPlayed}
+              order={order}
+            >
+Matches
+            </TableHeader>
           </tr>
         </thead>
         <tbody>
@@ -70,16 +124,14 @@ const LeaderboardPlayers = ({ playerSummaries }) => {
                   </Spacer>
                   {player.playerName}
                 </td>
-                <td className="small">
-                    slot
-                  {' '}
-                  {' '}
-                  {player.computed.team.teamId}
-                </td>
                 <td>{player.kills}</td>
-                <td>{Math.round(player.damage)}</td>
-                <td>{player.dbnos}</td>
-                <td>{player.headshotKills}</td>
+                <td>
+                  <BadgeStat value={player.computed.kd} great={player.computed.kd >= 2.5} good={player.computed.kd >= 1.5} />
+                </td>
+                <td>
+                  <BadgeStat value={player.computed.adr} great={player.computed.adr >= 300} good={player.computed.adr >= 200} />
+                </td>
+                <td>{player.computed.matchesPlayed}</td>
               </tr>
             ))
             }
