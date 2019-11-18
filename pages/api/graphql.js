f3b702374fef962117fcb0522f8bde55cf54ea04
António Santos <antoniojps@ua.pt>
2019-11-18 20:48:10 +0000

    Setup graphql api

---
+++ pages/api/graphql.js
@@ -0,0 +1,23 @@
+import { ApolloServer, gql } from 'apollo-server-micro';
+
+const typeDefs = gql`
+  type Query {
+    sayHello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    sayHello: () => 'Hello world',
+  },
+};
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default apolloServer.createHandler({ path: '/api/graphql' });
